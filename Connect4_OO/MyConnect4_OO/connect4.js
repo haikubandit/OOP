8c150bad94e0a2ae3b790f07dfdf9e9c323559a5
Daniel Knapp <knapparatus@gmail.com>
2021-01-26 16:11:42 -0600

    Added small improvements and Player class with color input

--- Connect4_OO/MyConnect4_OO/connect4.js
+++ Connect4_OO/MyConnect4_OO/connect4.js
@@ -6,13 +6,15 @@
  */
 
 class Game {
-	constructor(HEIGHT = 6, WIDTH = 7) {
+	constructor(p1, p2, HEIGHT = 6, WIDTH = 7) {
+		this.players = [ p1, p2 ];
 		this.WIDTH = WIDTH;
 		this.HEIGHT = HEIGHT;
-		this.currPlayer = 1; // active player: 1 or 2
+		this.currPlayer = p1; // active player: 1 or 2
 		this.board = []; // array of rows, each row is array of cells  (board[y][x])
 		this.makeBoard();
 		this.makeHtmlBoard();
+		this.gameOver = false;
 	}
 
 	/** makeBoard: create in-JS board structure:
@@ -27,13 +29,12 @@ class Game {
 	/** makeHtmlBoard: make HTML table and row of column tops. */
 	makeHtmlBoard() {
 		const board = document.getElementById('board');
+		board.innerHTML = '';
 
 		// make column tops (clickable area for adding a piece to that column)
 		const top = document.createElement('tr');
 		top.setAttribute('id', 'column-top');
 		top.addEventListener('click', this.handleClick.bind(this));
-		// top.addEventListener('click', this.handleClick);
-		console.log('makeHtmlBoard THIS => ', this);
 
 		for (let x = 0; x < this.WIDTH; x++) {
 			const headCell = document.createElement('td');
@@ -59,7 +60,6 @@ class Game {
 
 	/** findSpotForCol: given column x, return top empty y (null if filled) */
 	findSpotForCol(x) {
-		console.log('findSpotForCol THIS => ', this);
 		for (let y = this.HEIGHT - 1; y >= 0; y--) {
 			if (!this.board[y][x]) {
 				return y;
@@ -72,7 +72,8 @@ class Game {
 	placeInTable(y, x) {
 		const piece = document.createElement('div');
 		piece.classList.add('piece');
-		piece.classList.add(`p${this.currPlayer}`);
+		piece.style.backgroundColor = this.currPlayer;
+
 		piece.style.top = -50 * (y + 2);
 
 		const spot = document.getElementById(`${y}-${x}`);
@@ -86,10 +87,12 @@ class Game {
 
 	/** handleClick: handle click of column top to play piece */
 	handleClick(evt) {
+		// stop moves if gameOver = true;
+		if (this.gameOver) return alert('Game has ended! Click Start Game to play again.');
+
 		// get x from ID of clicked cell
 		const x = +evt.target.id;
 		// get next spot in column (if none, ignore click)
-		console.log('handleClick THIS => ', this);
 		const y = this.findSpotForCol(x);
 		if (y === null) {
 			return;
@@ -101,34 +104,31 @@ class Game {
 
 		// check for win
 		if (this.checkForWin()) {
+			this.gameOver = true;
 			return this.endGame(`Player ${this.currPlayer} won!`);
 		}
 
 		// check for tie
 		if (this.board.every(row => row.every(cell => cell))) {
+			this.gameOver = true;
 			return this.endGame('Tie!');
 		}
 
 		// switch players
-		this.currPlayer = this.currPlayer === 1 ? 2 : 1;
+		this.currPlayer = this.currPlayer === this.players[0] ? this.players[1] : this.players[0];
 	}
 
 	/** checkForWin: check board cell-by-cell for "does a win start here?" */
 	checkForWin() {
-		console.log('checkForWin THIS =>', this);
-		function _win(cells) {
+		const _win = cells => {
 			// Check four cells to see if they're all color of current player
 			//  - cells: list of four (y, x) cells
 			//  - returns true if all are legal coordinates & all match currPlayer
-
-			console.log('_win THIS =>', this);
-			console.log(cells);
-			console.log(this.HEIGHT);
 			return cells.every(
 				([ y, x ]) =>
 					y >= 0 && y < this.HEIGHT && x >= 0 && x < this.WIDTH && this.board[y][x] === this.currPlayer
 			);
-		}
+		};
 
 		for (let y = 0; y < this.HEIGHT; y++) {
 			for (let x = 0; x < this.WIDTH; x++) {
@@ -148,4 +148,16 @@ class Game {
 	}
 }
 
-new Game(); // assuming constructor takes height, width
+class Player {
+	constructor(color) {
+		this.color = color;
+	}
+}
+
+const startGame = document.getElementById('start-game');
+
+startGame.addEventListener('click', function() {
+	let p1 = document.getElementById('p1').value;
+	let p2 = document.getElementById('p2').value;
+	new Game(p1, p2); // assuming constructor takes height, width
+});
